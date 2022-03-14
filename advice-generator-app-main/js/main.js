e4697e3480fd5ac4b998fd096c8a786cae1667b6
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-14 17:20:24 -0500

    Add app fetch/UI update functions

---
+++ advice-generator-app-main/js/main.js
@@ -0,0 +1,24 @@
+const url = 'https://api.adviceslip.com/advice';
+const heading = document.getElementById('heading__text');
+const textBox = document.getElementById('advice__text');
+const btn = document.getElementById('advice__btn');
+
+async function fetchAdvice() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data.slip;
+}
+
+async function updateUI() {
+  const { id, advice } = await fetchAdvice();
+  heading.innerHTML = `Advice #${id}`;
+  textBox.innerHTML = `&ldquo;${advice}&rdquo;`;
+}
+
+async function handleUpdate() {
+  await updateUI();
+}
+
+// show advice on initial load
+updateUI();
