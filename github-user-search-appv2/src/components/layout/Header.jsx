75527cd5fb7ed72faf37c9e776fd1e13aa906f56
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-16 23:07:09 -0500

    Add Header component
    This will probably get a nested component that handles light/dark theme switching

---
+++ github-user-search-appv2/src/components/layout/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <div className='logo'>devfinder</div>
+      <div className='mode__changer'>Light Dark</div>
+    </header>
+  );
+}
+
+export default Header;
