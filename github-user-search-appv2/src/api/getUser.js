8fc29e8734afa577a1dd7010a3d66cfab5062d0c
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-16 23:33:57 -0500

    Add function to fetch api data

---
+++ github-user-search-appv2/src/api/getUser.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const GITHUB_URL = 'https://api.github.com/users/';
+
+const github = axios.create({
+  baseURL: GITHUB_URL,
+});
+
+export const getUser = async (text) => {
+  const response = await github.get(`${text}`);
+
+  return response.data;
+};
