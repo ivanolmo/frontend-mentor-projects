85c0d1f4f42ea8971dfa3c8a285261f35740ce8c
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-20 20:04:34 -0500

    Refactor to use context api and add remaining components
    This component is the main consumer

--- github-user-search-appv2/src/App.js
+++ github-user-search-appv2/src/App.js
@@ -1,11 +1,19 @@
-import { GithubProvider } from './context/GithubContext';
+import { useContext } from 'react';
+
+import GithubContext from './context/GithubContext';
+import Header from './components/layout/Header';
+import UserSearch from './components/layout/UserSearch';
 import Card from './components/layout/Card';
 
 function App() {
+  const { theme, user, isLoading, dispatch } = useContext(GithubContext);
+
   return (
-    <GithubProvider>
-      <Card />
-    </GithubProvider>
+    <main data-theme={theme}>
+      <Header theme={theme} dispatch={dispatch} />
+      <UserSearch dispatch={dispatch} />
+      <Card isLoading={isLoading} dispatch={dispatch} user={user} />
+    </main>
   );
 }
 
