879e54e1efd687c4857195ce138e303834a2ddc2
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-20 20:04:57 -0500

    Refactor to add main App component into context provider

--- github-user-search-appv2/src/index.js
+++ github-user-search-appv2/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import { GithubProvider } from './context/GithubContext';
+import './reset.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GithubProvider>
+      <App />
+    </GithubProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
