9424098a022482a37f244bac680635fc77879707
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-20 20:03:19 -0500

    Refactor to use contextapi
    Now passing most state data down as props

--- github-user-search-appv2/src/components/layout/Card.jsx
+++ github-user-search-appv2/src/components/layout/Card.jsx
@@ -1,27 +1,62 @@
-import { useContext } from 'react';
-import GithubContext from '../../context/GithubContext';
-import Header from './Header';
-import UserSearch from './UserSearch';
+import { useEffect } from 'react';
 
-function Card() {
-  const { isLoading } = useContext(GithubContext);
+import { getUser } from '../../context/GithubActions';
+import UserHeading from './user/UserHeading';
+import UserBio from './user/UserBio';
+import UserStats from './user/UserStats';
+import UserData from './user/UserData';
+import Spinner from './Spinner';
+
+function Card({ isLoading, dispatch, user }) {
+  useEffect(() => {
+    async function getFirstUser() {
+      dispatch({ type: 'SET_LOADING' });
+      const firstUser = await getUser('octocat');
+      dispatch({ type: 'GET_USER', payload: firstUser });
+    }
+    getFirstUser();
+  }, []);
+
+  const {
+    avatar_url,
+    name,
+    html_url,
+    login,
+    created_at,
+    bio,
+    public_repos,
+    followers,
+    following,
+    location,
+    blog,
+    twitter_username,
+    company,
+  } = user;
+
   return isLoading ? (
-    <p>loading</p>
+    <Spinner />
   ) : (
-    <>
-      <Header />
-      <UserSearch />
-      {/* 
-        inside card within card:
-          subheader (image, name, @, join date)
-          bio
-          repos/followers/following
-          loc/blog/twitter/work
-      
-      
-      */}
-      <div>da ist werk</div>
-    </>
+    <section className='card'>
+      <UserHeading
+        avatar={avatar_url}
+        name={name}
+        url={html_url}
+        login={login}
+        date={created_at}
+      />
+      <UserBio bio={bio} />
+      <UserStats
+        repos={public_repos}
+        followers={followers}
+        following={following}
+      />
+      <UserData
+        location={location}
+        blog={blog}
+        twitter_username={twitter_username}
+        company={company}
+      />
+    </section>
   );
 }
 
