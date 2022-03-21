9dcdd96a9b8545f631144fb40e9d7c3e424cc339
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-20 19:55:16 -0500

    Refactor Header component
    Now has theme toggle functionality
    Currently has a workaround to change body background-color

--- github-user-search-appv2/src/components/layout/Header.jsx
+++ github-user-search-appv2/src/components/layout/Header.jsx
@@ -1,8 +1,23 @@
-function Header() {
+import { ReactComponent as DarkIcon } from '../assets/icon-moon.svg';
+import { ReactComponent as LightIcon } from '../assets/icon-sun.svg';
+
+function Header({ theme, dispatch }) {
+  const switchTheme = () => {
+    dispatch({ type: 'TOGGLE_THEME', payload: theme });
+    // TODO temp work-around to change body color on theme switch
+    document.body.style.backgroundColor =
+      theme === 'light' ? '#141d2f' : '#f6f8ff';
+  };
+
   return (
     <header>
       <div className='logo'>devfinder</div>
-      <div className='mode__changer'>Light Dark</div>
+      <div className='mode__changer'>
+        <button className='theme__switcher' onClick={switchTheme}>
+          <span>{theme === 'light' ? 'Dark' : 'Light'}</span>
+          {theme === 'light' ? <DarkIcon /> : <LightIcon />}
+        </button>
+      </div>
     </header>
   );
 }
