a690011107105343473b3292a46ad185b76adfaa
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-21 22:39:02 -0500

    Update component to add inactive class if no data is present

--- github-user-search-appv2/src/components/layout/user/UserBio.jsx
+++ github-user-search-appv2/src/components/layout/user/UserBio.jsx
@@ -1,6 +1,6 @@
 function UserBio({ bio }) {
   return !bio ? (
-    <p className='user__bio'>This profile has no bio</p>
+    <p className='user__bio user__bio--inactive'>This profile has no bio</p>
   ) : (
     <section className='user__bio'>
       <p>{bio}</p>
