8022ecc97dbf5fad9d5a43a8ab8f62acb63f7ff8
Ivan Olmo <ivan.olmo@gmail.com>
2022-03-21 22:31:56 -0500

    Refactor component
    This refactor now renders a new component as a child
    Change was made to clean up repetitious code

--- github-user-search-appv2/src/components/layout/user/UserData.jsx
+++ github-user-search-appv2/src/components/layout/user/UserData.jsx
@@ -1,43 +1,24 @@
+import UserDataLine from './UserDataLine';
 import { ReactComponent as LocationIcon } from '../../assets/icon-location.svg';
 import { ReactComponent as WebsiteIcon } from '../../assets/icon-website.svg';
 import { ReactComponent as TwitterIcon } from '../../assets/icon-twitter.svg';
 import { ReactComponent as CompanyIcon } from '../../assets/icon-company.svg';
 
 function UserData({ location, blog, twitter_username, company }) {
-  // TODO icon color/transparency when no info is present
-
   return (
     <section className='user__data'>
-      <div className='data__line'>
+      <UserDataLine data={location}>
         <LocationIcon />
-        <span className={`line__link ${!location ? 'inactive' : ''}`}>
-          {location ? location : 'Not Available'}
-        </span>
-      </div>
-      <div className='data__line'>
+      </UserDataLine>
+      <UserDataLine data={blog}>
         <WebsiteIcon />
-        <a
-          href={`https://${blog}`}
-          className={`line__link ${!blog ? 'inactive' : ''}`}
-        >
-          {blog ? blog : 'Not Available'}
-        </a>
-      </div>
-      <div className='data__line'>
+      </UserDataLine>
+      <UserDataLine data={twitter_username}>
         <TwitterIcon />
-        <a
-          href={`https://twitter.com/${twitter_username}`}
-          className={`line__link ${!twitter_username ? 'inactive' : ''}`}
-        >
-          {twitter_username ? twitter_username : 'Not Available'}
-        </a>
-      </div>
-      <div className='data__line'>
+      </UserDataLine>
+      <UserDataLine data={company}>
         <CompanyIcon />
-        <span className={`line__link ${!company ? 'inactive' : ''}`}>
-          {company ? company : 'Not Available'}
-        </span>
-      </div>
+      </UserDataLine>
     </section>
   );
 }
